a2f00390b43bcb55cdc6e8ad291741042fa6886e
wave2day <wave@wave.today>
2026-05-07 03:43:50 +0200

    Update FMpanel.js

--- js/effects/fm/FMpanel.js
+++ js/effects/fm/FMpanel.js
@@ -1,19 +1,97 @@
-export function renderFMPanel() {
+export function createFMPanel(){
+
   return `
-    <div class="effect-settings fm-settings">
-      <div class="fm-title">FM MOD</div>
 
-      <label>Freq</label>
-      <input type="range" id="fmFreq" min="0.005" max="0.08" step="0.001" value="0.02">
+    <div class="panel"
+         data-panel="fm">
 
-      <label>Depth</label>
-      <input type="range" id="fmDepth" min="0" max="80" step="1" value="25">
+      <div class="fm-layout">
 
-      <label>Angle</label>
-      <input type="range" id="fmAngle" min="0" max="180" step="1" value="0">
+        <div class="fm-knobs">
 
-      <label>Threshold</label>
-      <input type="range" id="fmThreshold" min="0" max="255" step="1" value="128">
+          <div class="knob-row">
+
+            <div class="knob" data-knob="freq">
+              <div class="dial"></div>
+              <span>Freq</span>
+            </div>
+
+            <div class="knob" data-knob="depth">
+              <div class="dial"></div>
+              <span>Depth</span>
+            </div>
+
+          </div>
+
+          <div class="knob-row">
+
+            <div class="knob" data-knob="angle">
+              <div class="dial"></div>
+              <span>Angle</span>
+            </div>
+
+            <div class="knob" data-knob="flow">
+              <div class="dial"></div>
+              <span>Flow</span>
+            </div>
+
+          </div>
+
+          <div class="knob-row">
+
+            <div class="knob" data-knob="blend">
+              <div class="dial"></div>
+              <span>Blend</span>
+            </div>
+
+            <div class="knob" data-knob="colorize">
+              <div class="dial"></div>
+              <span>Color</span>
+            </div>
+
+          </div>
+
+        </div>
+
+        <div class="fm-slider-box">
+
+          <div class="fm-sliders">
+
+            <div class="v-slider-wrap">
+
+              <input type="range"
+                     id="fmSmooth"
+                     class="v-slider"
+                     min="0"
+                     max="1"
+                     step="0.001"
+                     value="0.5">
+
+              <span>Smooth</span>
+
+            </div>
+
+            <div class="v-slider-wrap">
+
+              <input type="range"
+                     id="fmThreshold"
+                     class="v-slider"
+                     min="0"
+                     max="1"
+                     step="0.001"
+                     value="0.5">
+
+              <span>Threshold</span>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
+
   `;
 }
