e4d325940daed0c5bde4792791cb765028eb39ff
wave2day <wave@wave.today>
2026-05-09 13:05:57 +0200

    Initialize appState with default values

---
+++ js/state/appState.js
@@ -0,0 +1,25 @@
+export const appState = {
+
+  activeTool: "drift",
+
+  activeEffects: [
+    "drift"
+  ],
+
+  paletteColor: "#ffffff",
+
+  transport: {
+
+    running: false,
+
+    progress: 0,
+
+    speed: 0.002
+  },
+
+  viewport: {
+
+    offsetX: 0,
+    offsetY: 0
+  }
+};
